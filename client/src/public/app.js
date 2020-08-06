20d1544da6d116655072ec5936bffaed7f8356fc
hanSong2 <rlagksd@naver.com>
2020-08-06 15:35:28 +0900

    [feat] 페이지 분리 및 옵저버 패턴 적용
    월 변경 시 모델 데이터 변경
    탭 변경 시 기존 데이터로 렌더링

--- client/src/public/app.js
+++ client/src/public/app.js
@@ -1,32 +1,11 @@
-import headerView from '../view/header/headerView.js';
-import NavigationView from '../view/navigation/navigationView.js';
-import HistoryView from '../view/history/historyView.js';
-import CalendarView from '../view/calendar/calendarView.js';
-import ChartView from '../view/chart/chartView';
-import RouterView from '../routerView';
-import router from '../router';
+import HKBPage from '../view/pages/HKBPage';
+import HeaderView from '../view/header/headerView.js';
 
 function render() {
   const headerWrapperDom = document.querySelector('.headerWrapper');
-  const naviWrapperDom = document.querySelector('.navigationWrapper');
-  const contentWrapperDom = document.querySelector('.contentWrapper');
-
-  new headerView(headerWrapperDom);
-  new NavigationView(naviWrapperDom);
-  contentWrapperDom.appendChild(new RouterView());
-  new ChartView(naviWrapperDom);
+  const page = document.querySelector('.page');
+  new HeaderView(headerWrapperDom);
+  new HKBPage(page);
 }
 
-function initialize() {
-  const contentWrapperDom = document.querySelector('.contentWrapper');
-  customElements.define('router-view', RouterView);
-  router.routes = [
-    { path: 'history', component: new HistoryView(contentWrapperDom) },
-    { path: 'calendar', component: new CalendarView(contentWrapperDom) },
-    { path: 'graph', component: new ChartView(contentWrapperDom) },
-  ];
-  render();
-  if (location.pathname !== 'calendar' && location.pathname !== 'graph') router.to('history');
-}
-
-initialize();
+render();
