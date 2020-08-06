3baa3fe142707b7597a914a72d73341ce23bd4ab
hanSong2 <rlagksd@naver.com>
2020-08-06 16:02:22 +0900

    [chore] 불필요한 파일 제거

--- client/src/view/calendar.js
+++
@@ -1,8 +0,0 @@
-export default class Calendar extends HTMLElement {
-  constructor() {
-    super();
-    this.innerHTML = `
-        <h1>calendar</h1>
-    `;
-  }
-}
--- client/src/view/graph.js
+++
@@ -1,8 +0,0 @@
-export default class Graph extends HTMLElement {
-  constructor() {
-    super();
-    this.innerHTML = `
-          <h1>graph</h1>
-      `;
-  }
-}
